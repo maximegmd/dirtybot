984b6b240ae892b7d629b859e26c3909425baf27
yamashi <yamashi@users.noreply.github.com>
2021-11-02 16:28:34 +0100

    Add logger

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,32 @@ mod config;
 mod blockchain;
 mod errors;
 
+fn setup_logger() -> Result<(), fern::InitError> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .chain(fern::log_file("bot.log")?)
+        .apply()?;
+    Ok(())
+}
+
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), fern::InitError> {
+
+    setup_logger()?;
 
     dotenv::dotenv().ok();
 
     let config = config::Config::init_from_env().unwrap();
 
-    println!("Hello, world!");
-
-    return ();
+    return Ok(());
 }
